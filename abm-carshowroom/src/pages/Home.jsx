d03f5a94f129b9f6529ada0a74ffe7b23ac47d97
alex1001A <alexiordanov1998@gmail.com>
2024-09-08 22:12:40 +0300

    select by brand

--- abm-carshowroom/src/pages/Home.jsx
+++ abm-carshowroom/src/pages/Home.jsx
@@ -6,21 +6,24 @@ import CarCard from "../components/CarCard";
 
 export default function Home() {
   const { vehicles, loading, error } = useVehicles();
-  const [selectedBrand, setSelectedBrand] = useState('');
+  const [selectedBrand, setSelectedBrand] = useState("");
+  const [selectedBrandSelect, setSelectedBrandSelect] = useState("");
 
-console.log(vehicles);
+  console.log(vehicles);
 
-
-// availabilityStatus : "In Stock"
+  // availabilityStatus : "In Stock"
 
   // Фильтрация по выбранному бренду
-  const filteredVehicles = selectedBrand
-    ? vehicles.filter(vehicle => vehicle.brand === selectedBrand)
-    : vehicles;
+  const filteredVehicles = selectedBrand ? vehicles.filter((vehicle) => vehicle.brand === selectedBrand) : vehicles;
+  const filteredVehiclesBySelect = selectedBrandSelect ? vehicles.filter((vehicle) => vehicle.brand === selectedBrandSelect) : vehicles;
 
   const handleBrandFilter = (brand) => {
     setSelectedBrand(brand);
   };
+
+  const handleBrandFilterSelect = (e) => {
+    setSelectedBrandSelect(e.target.value)
+  }
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error loading vehicles.</div>;
@@ -28,16 +31,22 @@ console.log(vehicles);
   return (
     <div>
       <h1>Car List</h1>
-      
+
       {/* Фильтрация  */}
       <div className="filter-controls">
-        <button onClick={() => handleBrandFilter('Chrysler')}>Chrysler</button>
-        <button onClick={() => handleBrandFilter('Dodge')}>Dodge</button>
-        <button onClick={() => setSelectedBrand('')}>Show All</button>
+        <label for="car-select">Choose a pet:</label>
+        <select onChange={handleBrandFilterSelect} name="cars" id="car-select">
+          <option value="">--Please choose an option--</option>
+          <option value="Chrysler">Chrysler</option>
+          <option value="Dodge">Dodge</option>
+        </select>{" "}
+        <button onClick={() => handleBrandFilter("Chrysler")}>Chrysler</button>
+        <button onClick={() => handleBrandFilter("Dodge")}>Dodge</button>
+        <button onClick={() => setSelectedBrand("")}>Show All</button>
       </div>
-      
+
       <div className="car-list">
-        {filteredVehicles.map(vehicle => (
+        {filteredVehiclesBySelect.map((vehicle) => (
           <Link key={vehicle.id} to={`/vehicles/${vehicle.id}`} className="car-card-link">
             <CarCard vehicle={vehicle} />
           </Link>
