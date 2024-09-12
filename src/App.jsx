acf1fc23b1e856b46d8e2e8375e3a576592b3641
alex1001A <alexiordanov1998@gmail.com>
2024-09-12 22:38:04 +0300

    change host

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import CarDetails from "./components/CarDetails";
 export default function App() {
   return (
     <Routes>
-      <Route path="/vehicles" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/vehicles/:vehicleId" element={<CarDetails />} />
     </Routes>
   );
